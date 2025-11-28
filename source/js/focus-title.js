6d4c646dd6a10f4595ac19bef05405d9ff42b1ac
Hetian <1009843097@qq.com>
2025-11-28 20:57:45 +0800

    refactor: 注释掉焦点标题脚本，暂时禁用功能

--- source/js/focus-title.js
+++ source/js/focus-title.js
@@ -1,12 +1,12 @@
-(() => {
-    let oldTitle = document.title;
-    document.addEventListener("visibilitychange", function () {
-            if (document.hidden) {
-                oldTitle = document.title;
-                document.title = "诶!? 不许跑喵~";
-            } else {
-                document.title = oldTitle;
-            }
-        }
-    );
-})();
+// (() => {
+//     let oldTitle = document.title;
+//     document.addEventListener("visibilitychange", function () {
+//             if (document.hidden) {
+//                 oldTitle = document.title;
+//                 document.title = "诶!? 不许跑喵~";
+//             } else {
+//                 document.title = oldTitle;
+//             }
+//         }
+//     );
+// })();
